61f3e732f15245767e116de32f0cac92ea92b0f6
Edrixx <edrixx.yt02@gmail.com>
2025-12-12 18:59:02 +0900

    ImageUploader change UI

--- components/ImageUploader.vue
+++ components/ImageUploader.vue
@@ -82,63 +82,34 @@ const onReplaceFileChange = (event: Event) => {
       <div
         v-for="(img, idx) in previewImages"
         :key="idx"
-        class="group relative aspect-square bg-gray-800 rounded-lg border border-gray-700 overflow-hidden hover:border-blue-500 transition-colors"
+        @click="triggerReplace(idx)"
+        class="group relative aspect-square bg-gray-800 rounded-lg border border-gray-700 overflow-hidden hover:border-blue-500 transition-all cursor-pointer hover:opacity-90"
+        title="Click to replace image"
       >
         <img :src="img" class="w-full h-full object-cover" />
 
+        <button
+          v-if="previewImages.length > 1"
+          @click.stop="emit('remove-image', idx)"
+          class="absolute top-1 right-1 p-1 rounded-full bg-black/50 text-gray-300 hover:bg-red-600 hover:text-white transition-colors z-20 backdrop-blur-sm"
+          title="Remove image"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="w-3.5 h-3.5"
+          >
+            <path
+              d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
+            />
+          </svg>
+        </button>
+
         <div
-          class="absolute bottom-0 right-0 bg-blue-600/90 text-white text-[10px] font-bold px-1.5 py-0.5 rounded-tl-md z-10"
+          class="absolute bottom-0 right-0 bg-blue-600/90 text-white text-[10px] font-bold px-1.5 py-0.5 rounded-tl-md z-10 pointer-events-none"
         >
           #{{ idx + 1 }}
-        </div>
-
-        <div
-          class="absolute inset-0 bg-black/60 opacity-0 group-hover:opacity-100 transition-opacity flex flex-col items-center justify-center gap-2 backdrop-blur-[2px]"
-        >
-          <button
-            @click="triggerReplace(idx)"
-            class="p-1.5 rounded-full bg-gray-700 hover:bg-blue-600 text-white transition-colors"
-            title="Replace image"
-          >
-            <svg
-              class="w-4 h-4"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
-              ></path>
-            </svg>
-          </button>
-
-          <button
-            @click="emit('remove-image', idx)"
-            class="p-1.5 rounded-full bg-gray-700 hover:bg-red-600 text-white transition-colors"
-            title="Remove image"
-            :disabled="previewImages.length <= 1"
-            :class="
-              previewImages.length <= 1
-                ? 'opacity-50 cursor-not-allowed hover:bg-gray-700'
-                : ''
-            "
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="w-4 h-4"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 001.5.06l.3-7.5z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </button>
         </div>
       </div>
 
